853abc53cf21045bac8e77e25fcc4baf0d5cba46
Sdanisjb <31196541+Sdanisjb@users.noreply.github.com>
2017-09-22 08:31:27 -0500

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,181 @@
+#include <windows.h>
+
+#include <iostream>
+
+#include <stdio.h>
+
+#include <stdlib.h>
+
+#include <conio.h>
+
+using namespace std;
+
+
+
+int main()
+
+{
+    //DO Beep(261,200);
+    //RE Beep(293,200);
+    //MI Beep(329,200);
+    //FA Beep(349,200);
+    //SOL Beep(392,200);
+    //LA Beep(440,200);
+    //SI Beep(493,200);
+    //DO A Beep(523,200);
+    //RE A Beep(587,200);
+    //MI A Beep(659,200);
+    //FA A Beep(698,200);
+    //SOL A Beep(784,200);
+    //DO# Beep(277,200);
+    //RE# Beep(311,200);
+    //FA# Beep(370,200);
+    //SOL# Beep(415,200);
+    //LA# Beep(466,200);
+    //DO# A Beep(529,200);
+    //RE# A Beep(561,200);
+
+    while(1){
+
+
+
+    Beep(493,200);
+    Beep(587,200);
+    Beep(659,200);
+    Beep(784,200);
+    Beep(784,200);
+    Beep(370,200);
+    Beep(784,200);
+    Beep(370,200);
+    Beep(784,200);
+    Beep(370,200);
+    Beep(659,200);
+
+    Sleep(100);
+
+    Beep(493,200);
+    Beep(587,200);
+    Beep(659,200);
+    Beep(784,200);
+    Beep(784,200);
+    Beep(370,200);
+    Beep(784,200);
+    Beep(370,200);
+    Beep(784,200);
+    Beep(370,200);
+    Beep(659,200);
+    Beep(523,200);
+
+    Sleep(100);
+
+    Beep(440,200);
+    Beep(784,200);
+    Beep(370,200);
+    Beep(659,200);
+    Beep(587,200);
+    Beep(523,200);
+    Beep(587,200);
+    Beep(587,300);
+    Beep(440,200);
+    Beep(784,200);
+    Beep(370,200);
+    Beep(659,200);
+    Beep(587,200);
+    Beep(523,200);
+    Beep(587,200);
+
+    Sleep(100);
+
+    Beep(587,400);
+    Beep(784,200);
+    Beep(440,200);
+    Beep(466,200);
+    Beep(493,300);
+    Beep(493,200);
+    Beep(529,200);
+    Beep(587,200);
+    Beep(603,500);
+
+    Sleep(100);
+
+    Beep(311,500);
+    Beep(523,200);
+    Beep(493,200);
+    Beep(440,200);
+    Beep(392,200);
+
+    Sleep(100);
+
+    Beep(493,200);
+    Beep(587,200);
+    Beep(659,200);
+    Beep(784,200);
+    Beep(784,200);
+    Beep(370,200);
+    Beep(784,200);
+    Beep(370,200);
+    Beep(784,200);
+    Beep(370,200);
+    Beep(659,200);
+
+    Sleep(100);
+
+    Beep(493,200);
+    Beep(587,200);
+    Beep(659,200);
+    Beep(784,200);
+    Beep(784,200);
+    Beep(370,200);
+    Beep(784,200);
+    Beep(370,200);
+    Beep(784,200);
+    Beep(370,200);
+    Beep(659,200);
+    Beep(523,200);
+
+    Sleep(100);
+
+    Beep(440,200);
+    Beep(784,200);
+    Beep(370,200);
+    Beep(659,200);
+    Beep(587,200);
+    Beep(523,200);
+    Beep(587,200);
+    Beep(587,300);
+    Beep(440,200);
+    Beep(784,200);
+    Beep(370,200);
+    Beep(659,200);
+    Beep(587,200);
+    Beep(523,200);
+    Beep(587,200);
+
+    Sleep(100);
+
+    Beep(587,400);
+    Beep(784,200);
+    Beep(440,200);
+    Beep(466,200);
+    Beep(493,300);
+    Beep(493,200);
+    Beep(529,200);
+    Beep(587,200);
+    Beep(603,500);
+
+    Sleep(100);
+
+    Beep(493,200);
+    Beep(493,200);
+    Beep(529,200);
+    Beep(561,200);
+    Beep(659,200);Sleep(100);
+
+    Sleep(100);
+
+    }
+
+    return 0;
+
+}
+
